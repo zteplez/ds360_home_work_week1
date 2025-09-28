e424348e62f3380ca2ab93bd1f24a19b60702b7a
zteplez <asibrahim98@gmail.com>
2025-09-28 16:14:25 +0300

    [feat]: Implement iris download function

---
+++ src/download_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import seaborn as sns
+import os
+
+def download_iris_data():
+    df = sns.load_dataset("iris")
+
+    df.to_csv('data/raw/iris.csv', index=False)
+
+    print("Iris data seti indirildi ?")
+    print(f"Veri boyutu: {df.shape}")
+    print(f"Kolonlar: {list(df.columns)}")
+    print(f"Eksik değerler:\n{df.isnull().sum()}")
+
+if __name__ == "__main__":
+    download_iris_data()
